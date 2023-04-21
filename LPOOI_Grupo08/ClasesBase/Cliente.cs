9266aa6a5e2eeac6d24252eaeb8a67c0cd84890b
carlos sanchez <argcarlos688@gmail.com>
2023-04-21 01:12:51 -0300

    Agregando clase Cliente

---
+++ LPOOI_Grupo08/ClasesBase/Cliente.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    public class Cliente
+    {
+        private string cli_Dni;
+
+        public string Cli_Dni
+        {
+            get { return cli_Dni; }
+            set { cli_Dni = value; }
+        }
+        private string cli_Apellido;
+
+        public string Cli_Apellido
+        {
+            get { return cli_Apellido; }
+            set { cli_Apellido = value; }
+        }
+        private string cli_Nombre;
+
+        public string Cli_Nombre
+        {
+            get { return cli_Nombre; }
+            set { cli_Nombre = value; }
+        }
+        private string cli_Direccion;
+
+        public string Cli_Direccion
+        {
+            get { return cli_Direccion; }
+            set { cli_Direccion = value; }
+        }
+        private string cli_Cuit;
+
+        public string Cli_Cuit
+        {
+            get { return cli_Cuit; }
+            set { cli_Cuit = value; }
+        }
+        private string cli_NroCarnet;
+
+        public string Cli_NroCarnet
+        {
+            get { return cli_NroCarnet; }
+            set { cli_NroCarnet = value; }
+        }
+        
+
+
+    }
+}
