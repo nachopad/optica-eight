124f56a6a5568dc7272159eef7c9cbfa4cc8f6b8
DESKTOP-EIGD948\PC <kevinpaco40@gmail.com>
2023-04-21 11:02:31 -0300

    se agrega Roles

---
+++ LPOOI_Grupo08/ClasesBase/Roles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    public class Roles
+    {
+        private int rol_Codigo;
+
+        public int Rol_Codigo
+        {
+            get { return rol_Codigo; }
+            set { rol_Codigo = value; }
+        }
+        private string rol_Descripcion;
+
+        public string Rol_Descripcion
+        {
+            get { return rol_Descripcion; }
+            set { rol_Descripcion = value; }
+        }
+    }
+}
