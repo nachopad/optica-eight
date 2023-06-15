610b0211fde7620696f0d25cefec9591254c6f18
MaxiCarrillo <103146282+MaxiCarrillo@users.noreply.github.com>
2023-06-14 21:21:51 -0300

    Correcion Program.cs

--- LPOOI_Grupo08/Vistas/Program.cs
+++ LPOOI_Grupo08/Vistas/Program.cs
@@ -15,7 +15,7 @@ namespace Vistas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            Application.Run(new formLogin());
         }
     }
 }
